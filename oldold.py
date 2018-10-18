44f6311924e48f7c37be564c0fd3249a2aa78ec1
Robert Charlton <robcharlton@sap.com>
2018-10-18 12:29:57 +1100

    .

--- oldold.py
+++ oldold.py
@@ -8,8 +8,6 @@ BW_LIMIT = int(sys.argv[2])
 MB_THRESHOLD = int(sys.argv[3])
 FILE_NAME = sys.argv[4]
 
-points_from_line = []
-
 def save_image( npdata, filename ) :
     img = Image.fromarray( npdata.astype('uint8')*255, "L" )
     img.save( filename )
@@ -389,7 +387,7 @@ def startRemovingBasedOnMiddle(top_threshold, bottom_threshold, middle_threshold
     while column_position < len(original_matrix[middle_threshold]) - 1:
         if original_matrix[middle_threshold][column_position] == 0:
             fillAndDelete2(original_matrix, middle_threshold, top_threshold, bottom_threshold, column_position)
-            #save_image(original_matrix, "1.jpeg")
+            save_image(original_matrix, "1.jpeg")
         column_position = column_position + 1
 
     column_position = 0
@@ -400,7 +398,7 @@ def startRemovingBasedOnMiddle(top_threshold, bottom_threshold, middle_threshold
                 (firstBoxLeft, firstBoxTop, firstBoxRight, firstBoxBottom, secondBoxLeft, secondBoxTop, secondBoxRight, secondBoxBottom) = calculateBox(matrix_copy, row_position, top_threshold, bottom_threshold, column_position)
                 removePotentialLinePixelsInBox(original_matrix, firstBoxLeft, firstBoxTop, firstBoxRight, firstBoxBottom, matrix_copy)
                 removePotentialLinePixelsInBox(original_matrix, secondBoxLeft, secondBoxTop, secondBoxRight, secondBoxBottom, matrix_copy)
-                #save_image(original_matrix, "2.jpeg")
+                save_image(original_matrix, "2.jpeg")
             row_position = row_position + 1
         column_position = column_position + 1
 
@@ -533,44 +531,44 @@ def twoContinuousOfLess2SegmentsInBox(boxCirumference, boxSize):
         temp = boxCirumference[lastIndex]
         del boxCirumference[lastIndex]
         boxCirumference = [temp] + boxCirumference
-    #print(boxCirumference)
+    print(boxCirumference)
     currentContCount = 0
     currentSegmentCount = 0
     currentlyBlack = False
     boxLength = len(boxCirumference)
-    #print "boxlen - %d" % (boxLength)
+    print "boxlen - %d" % (boxLength)
     lengthThreshold = boxSize
     lengthCounter = 0
     boxIndex = 0
     firstSegmentFound = False
     while boxIndex < boxLength - 1:
-        #print "box index = %d" % (boxIndex)
+        print "box index = %d" % (boxIndex)
         if not currentlyBlack:
-            #print "not black"
+            print "not black"
             if boxCirumference[boxIndex] == 0:
-                #print "found first black"
+                print "found first black"
                 currentlyBlack = True
                 currentSegmentCount = currentSegmentCount + 1
                 if currentSegmentCount > 2:
-                    #print "false cause more segments"
+                    print "false cause more segments"
                     return False
                 currentContCount = currentContCount + 1
                 if lengthCounter < lengthThreshold and firstSegmentFound:
-                    #print "FALSE"
+                    print "FALSE"
                     return False
             else:
                 if firstSegmentFound:
-                    #print "increase it"
+                    print "increase it"
                     lengthCounter = lengthCounter + 1
         else:
             if boxCirumference[boxIndex] == 0:
-                #print "conintue black"
+                print "conintue black"
                 currentContCount = currentContCount + 1
                 if currentContCount > 2:
-                    #print "false for contunity count"
+                    print "false for contunity count"
                     return False
             else:
-                #print "now not black"
+                print "now not black"
                 currentlyBlack = False
                 currentContCount = 0
                 firstSegmentFound = True
@@ -609,13 +607,13 @@ def check_line_candidate(matrix, y, x):
     boxThreshold = 11
     boxCirumference = makeBoxCircumferenceForPixel(matrix, y, x, boxSize)
     theBoxTotal = boxTotal(matrix, y, x, boxSize)
-    #print(boxCirumference)
-    #print(theBoxTotal)
+    print(boxCirumference)
+    print(theBoxTotal)
     if theBoxTotal < boxThreshold and theBoxTotal > boxSize:
-        #print "here"
+        print "here"
         if twoContinuousOfLess2SegmentsInBox(boxCirumference, boxSize):
             bt = connectedInBoxTotal(matrix, y, x, boxSize)
-            #print "oh...... potential with total %d:%d" % (theBoxTotal, bt)
+            print "oh...... potential with total %d:%d" % (theBoxTotal, bt)
             #if connectedInBoxTotal(matrix, y, x, boxSize) == theBoxTotal:
             return True
     return False
@@ -628,8 +626,6 @@ def removeLine(matrix, y, x):
     threshold_to_not_exceed = 5
     box_size = 6
     matrix[y][x] = white
-    global points_from_line
-    points_from_line = points_from_line + [(y,x)]
     #print(" deleting %0d:%d" % (y, x))
     if x > 0:
         if matrix[y][x-1] == black and blackPixelBoxThresholdNotExceeded(matrix, x-1, y, threshold_to_not_exceed, box_size):
@@ -658,7 +654,7 @@ def removeLines(matrix):
         while row_index < (width-4):
             if matrix_copy[column_index][row_index] == 0:
                 if check_line_candidate(matrix_copy, column_index, row_index):
-                    #print(" deleting %0d:%d" % (column_index, row_index))
+                    print(" deleting %0d:%d" % (column_index, row_index))
                     removeLine(matrix_copy, column_index, row_index)
             row_index = row_index+1
         column_index = column_index + 1
@@ -741,40 +737,24 @@ def getNumberOriginalBlackPixels(new_matrix, original_matrix):
         column_index = column_index + 1
     return  blackCount
 
-def getNumberOfTransitions(matrix, original_matrix):
+def getNumberOfTransitions(matrix):
     height = len(matrix)
     width = len(matrix[0])
     column_index = 0
     transitionCount = 0
-    
+    currentlyBlack = False
     while column_index < (height-1):
         row_index = 0
-        current_row_transition_count = 0
-        currentlyBlack = False
-        
-        firstFound = False
         while row_index < (width-1):
-            
             if matrix[column_index][row_index] == 0:
-
-                if not firstFound:
-                    firstFound = True
-                    if original_matrix[column_index][row_index] == 0:
-                        currentlyBlack = True
-                    else:
-                        currentlyBlack = False
-
-                if original_matrix[column_index][row_index] == 0:
-                    if not currentlyBlack:
-                        transitionCount = transitionCount + 1
-                        currentlyBlack = True
-                    else:
-                        if currentlyBlack:
-                            currentlyBlack = False
-
+                if not currentlyBlack:
+                    transitionCount = transitionCount + 1
+                    currentlyBlack = True
+            else:
+                if currentlyBlack:
+                    currentlyBlack = False
             row_index = row_index+1
         column_index = column_index + 1
-        transitionCount = transitionCount + current_row_transition_count
     return  transitionCount    
 
 def getBWmatrix():
@@ -785,10 +765,19 @@ def getBWmatrix():
 def fillMatrix(matrix, y, x):
     height = len(matrix)
     width = len(matrix[0])
-    #print("x: %d y: %d" % (x, y))
-    #print("fill")
-    #print("%d %d %d %d" % (matrix[y][x-1], matrix[y][x+1], matrix[y-1][x], matrix[y+1][x]))
+    print("x: %d y: %d" % (x, y))
+    print("fill")
+    print("%d %d %d %d" % (matrix[y][x-1], matrix[y][x+1], matrix[y-1][x], matrix[y+1][x]))
     matrix[y][x] = 2
+    
+    #if matrix[y][x-1] == 0:
+    #    fillMatrix(matrix, y, x-1)
+    #if matrix[y][x+1] == 0:
+    #    fillMatrix(matrix, y, x+1)
+    #if matrix[y-1][x] == 0:
+    #    fillMatrix(matrix, y-1, x)
+    #if matrix[y+1][x] == 0:
+    #    fillMatrix(matrix, y+1, x)
 
     foundOne = False
     while(True):
@@ -798,7 +787,7 @@ def fillMatrix(matrix, y, x):
             row_index = 0
             while row_index < (width-1):
                 if matrix[column_index][row_index] == 2:
-                    #print("Found a two %d %d" % (column_index, row_index))
+                    print("Found a two %d %d" % (column_index, row_index))
                     # Check all around it and make it 2's
                     if matrix[column_index][row_index-1] == 0:
                         matrix[column_index][row_index-1] = 2
@@ -815,7 +804,16 @@ def fillMatrix(matrix, y, x):
                 row_index = row_index+1
             column_index = column_index + 1
         if row_index == width-1 and column_index == height-1:
-            return 
+            return
+
+    #if x-1 >= leftBoundary and y-1 >= topBoundary and matrix[y-1][x-1] == 0:
+    #    fillMatrix(matrix, y-1, x-1)
+    #if x-1 >= leftBoundary and y+1 >= bottomBoundary and matrix[y][x-1] == 0:
+    #    fillMatrix(matrix, y+1, x-1)
+    #if x+1 <= rightBoundary and y-1 >= topBoundary and matrix[y][x+1] == 0:
+    #    fillMatrix(matrix, y-1, x+1)
+    #if x+1 <= rightBoundary and  y+1 >= bottomBoundary and matrix[y][x+1] == 0:
+        #fillMatrix(matrix, y+1, x+1)   
 
 def set_to_zero(matrix):
     height = len(matrix)
@@ -824,7 +822,7 @@ def set_to_zero(matrix):
     while column_index < (height-1):
         row_index = 0
         while row_index < (width-1):
-            matrix[column_index][row_index] = 1
+            matrix[column_index][row_index] = 0
             row_index = row_index+1
         column_index = column_index + 1
 
@@ -838,82 +836,21 @@ def changeAllButTwo(matrix):
             if not (matrix[column_index][row_index] == 2):
                 matrix[column_index][row_index] = 1
             if (matrix[column_index][row_index] == 2):
+                print("found a 2")
                 matrix[column_index][row_index] = 0
             row_index = row_index+1
         column_index = column_index + 1
 
-def checkIfShouldBeAdded(matrix, original_matrix):
-    delta_x = getCalculations(matrix, original_matrix)
-    if delta_x > 200:
-        return True
-    else:
-        return False
-
 def getCalculations(matrix, original_matrix):
-    T = getNumberOfTransitions(matrix, original_matrix)
-    BC = getBlackPixelCount(matrix)
-    DC = getNumberOriginalBlackPixels(matrix, original_matrix)
-    (mi_y, ma_y) = getMinYandYlength(matrix)
-    delta_y = ma_y-mi_y
-    (mi_x, ma_x) = getMinYandYlength(matrix)
-    delta_x = ma_x-mi_x
-    
-    e  = 0
-    s = 0
-    Rm = 0
-    if not (delta_y == 0):
-        e = delta_x/delta_y
-    else:
-        e = 0
+    print(getBlackPixelCount(matrix))
+    print(getMinXandXlength(matrix))
+    print(getMinYandYlength(matrix))
+    print(getNumberOriginalBlackPixels(matrix, original_matrix))
+    print(getNumberOfTransitions(matrix))
 
-    if not (e == 0):
-        s = int(BC/e)
-    else:
-        s = 0
-
-    if not (DC == 0):
-        Rm = int(BC/DC)
-    else:
-        Rm = 0
-
-    print("###############################################")
-    print("T = %d" % (T))
-    print("BC = %d" % (BC))
-    print("DC = %d" % (DC))
-    print("y = %d"  % (delta_y))
-    print("x = %d"  % (delta_x))
-    print("s = %d"  % (s))
-    print("e = %d"  % (e))
-    print("Rm = %d"  % (Rm))
-    return delta_x
-
-def drawOnPiece(canvas, matrix_copy):
-    height = len(matrix_copy)
-    width = len(matrix_copy[0])
-    column_index = 0
-    while column_index < (height-1):
-        row_index = 0
-        while row_index < (width-1):
-            if matrix_copy[column_index][row_index] == 0:
-                canvas[column_index][row_index] = 0
-            row_index = row_index + 1
-        column_index = column_index + 1
-
-def deleteMatrixFromMatrix(map_matrix, matrix):
-    height = len(matrix)
-    width = len(matrix[0])
-    column_index = 0
-    while column_index < (height-1):
-        row_index = 0
-        while row_index < (width-1):
-            if map_matrix[column_index][row_index] == 0:
-                matrix[column_index][row_index] = 1
-            row_index = row_index + 1
-        column_index = column_index + 1
-
-def loopThroughConnectedComponents(matrix, original_matrix):
-    matrix_of_pieces_to_keep = deepcopy(matrix)
-    set_to_zero(matrix_of_pieces_to_keep)
+def loopThroughConnectedComponents(matrix):
+    matrix_copy_to_return = deepcopy(matrix)
+    set_to_zero(matrix_copy_to_return)
     matrix_copy = deepcopy(matrix) 
     height = len(matrix)
     width = len(matrix[0])
@@ -923,40 +860,17 @@ def loopThroughConnectedComponents(matrix, original_matrix):
         while row_index < (width-1):
             if matrix_copy[column_index][row_index] == 0:
                 fillMatrix(matrix_copy, column_index, row_index) # We are just doing one connected component for now
+                print("after fill")
                 changeAllButTwo(matrix_copy)
-                #save_image(matrix_copy, "after_two.jpeg")
-                if checkIfShouldBeAdded(matrix_copy, original_matrix):
-                    drawOnPiece(matrix_of_pieces_to_keep, matrix_copy)
-                #
-                # Delete this from original matrix
-                deleteMatrixFromMatrix(matrix_copy, matrix)
-                matrix_copy = deepcopy(matrix)
-                #
-                #return matrix_of_pieces_to_keep
+                save_image(matrix_copy, "after_two.jpeg")
+                print("after chgange")
+                getCalculations(matrix_copy, matrix)
+                # If calculations are not text, take it out, as in white out the pixels in the original image
+                # then for our deleted line before we should remember what it is to put it back
+                return
             row_index = row_index+1
-        column_index = column_index + 1
-    return matrix_of_pieces_to_keep
+        column_index = column_index + 1  
 
-def drawPiecesOntoCanvas(canvas_matrix, map_matrix, original_matrix):
-    height = len(original_matrix)
-    width = len(original_matrix[0])
-    column_index = 0
-    while column_index < (height-1):
-        row_index = 0
-        while row_index < (width-1):
-            if map_matrix[column_index][row_index] == 0:
-                if original_matrix[column_index][row_index] == 0:
-                    canvas_matrix[column_index][row_index] = 0
-                else:
-                    canvas_matrix[column_index][row_index] = 1
-            else:
-                canvas_matrix[column_index][row_index] = 1
-            row_index = row_index+1
-        column_index = column_index + 1
-
-def drawLineBackOne(matrix):
-    for (y,x) in points_from_line:
-        matrix[y][x] = 0
 
 ####################################################
 # MAIN
@@ -968,26 +882,24 @@ lineRemovedMatrix = removeLines(bwMatrix)
 save_image(lineRemovedMatrix, "after_lines_removed.jpeg")
 
 # Step Two: Do RLSA
-mod_matrix = doHorizontalEditingAndReturnNewMatrix(lineRemovedMatrix, 120)
+mod_matrix = doHorizontalEditingAndReturnNewMatrix(lineRemovedMatrix, 200)
 save_image(mod_matrix, "after_hor.jpeg")
-mod_matrix2 = doVerticalEditingAndReturnNewMatrix(lineRemovedMatrix, 500)
+mod_matrix2 = doVerticalEditingAndReturnNewMatrix(lineRemovedMatrix, 120)
 save_image(mod_matrix2, "after_ver.jpeg")
 final_matrix = andThem(mod_matrix, mod_matrix2, width, height)
 save_image(final_matrix, "final.jpeg")
 
 # Step Three: Loop through connected components
 print("here")
-matrix_of_pieces_to_delete = loopThroughConnectedComponents(final_matrix, lineRemovedMatrix)
-save_image(matrix_of_pieces_to_delete, "matrix_of_pieces_to_delete.jpeg")
+loopThroughConnectedComponents(final_matrix)
 
-#drawLineBackOne(matrix_to_remove_line)
-#save_image(matrix_to_remove_line, "matrix_to_remove_line1.jpeg")
-deleteMatrixFromMatrix(matrix_of_pieces_to_delete, bwMatrix)
-save_image(bwMatrix, "matrix_to_remove_line2.jpeg")
+##
+#run(bwMatrix, bwMatrix)
+#save_image(bwMatrix, "after_run4.jpeg")
+#runHoritontal(bwMatrix, bwMatrix)
+#save_image(bwMatrix, "after_runHorizontal.jpeg")
+##
 
-# Step Four: Finally run remove line
-run(bwMatrix, bwMatrix)
-save_image(bwMatrix, "complete.jpeg")
 
 
 
